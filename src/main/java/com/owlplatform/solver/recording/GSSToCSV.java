831c9ce92ba41943f36f7915a74bc24aa180ffa0
Robert Moore <rob@owlplatform.com>
2014-07-27 23:06:36 -0400

    Changing format to print pip data

--- src/main/java/com/owlplatform/solver/recording/GSSToCSV.java
+++ src/main/java/com/owlplatform/solver/recording/GSSToCSV.java
@@ -48,12 +48,126 @@ import com.owlplatform.common.util.NumericUtils;
  * 
  */
 public class GSSToCSV {
+
+	private static final class PipSamplePrinter {
+
+		public static String sampleToString(final ReplaySample sample) {
+			if (sample.getPhysicalLayer() != SampleMessage.PHYSICAL_LAYER_PIPSQUEAK) {
+				return null;
+			}
+			final StringBuilder sb = new StringBuilder(80);
+
+			sb.append(Long.toString(sample.getReplayTime()));
+			sb.append(',');
+			sb.append(Long.toString(sample.getReceiverTimeStamp()));
+			sb.append(',');
+			sb.append(Integer.toString(sample.getPhysicalLayer() & 0xFF));
+			sb.append(',');
+			byte[] idBytes = sample.getDeviceId();
+			int devId = (idBytes[idBytes.length - 3] << 16) & 0xff0000;
+			devId += (idBytes[idBytes.length - 2] << 8) & 0xff00;
+			devId += (idBytes[idBytes.length - 1]) & 0xff;
+			sb.append(devId);
+			sb.append(',');
+			idBytes = sample.getReceiverId();
+			int recId = (idBytes[idBytes.length - 3] << 16) & 0xff0000;
+			recId += (idBytes[idBytes.length - 2] << 8) & 0xff00;
+			recId += (idBytes[idBytes.length - 1]) & 0xff;
+			sb.append(recId);
+			sb.append(',');
+			sb.append(String.format(THREE_DECIMAL_FLOAT, sample.getRssi()));
+			sb.append(',');
+			if (sample.getSensedData() != null) {
+				sb.append(PipSamplePrinter.decodeData(sample.getSensedData()));
+			}
+
+			return sb.toString();
+		}
+
+		public static String decodeData(final byte[] data) {
+			// No data, or header with no values, or "decode" header
+			if (data == null || data.length < 2 || (data[0] & 0x80) == 0x80) {
+				return NumericUtils.toHexString(data);
+			}
+
+			int offset = 0;
+			final byte header = data[0];
+			int temp7 = -400;
+			float temp16 = -400f;
+			boolean isBool = false;
+			int light = -1;
+			int battery_volt = -1;
+			int battery_joule = -1;
+			try {
+				if (((header & 0x01) == 0x01) && (offset + 1) < data.length) {
+					// 7-bit temp + binary
+					++offset;
+					temp7 = (data[offset] >> 1) - 40;
+					isBool = (data[offset] & 0x01) == 0x01;
+				}
+				// 16-bit temp
+				if (((header & 0x02) == 0x02) && (offset + 2) < data.length) {
+					++offset;
+
+					int frac = data[offset + 1] & 0x0F;
+					int whole = data[offset + 1] >> 4;
+					whole += (data[offset] << 4) & 0x0FF0;
+					temp16 = (whole - 40) + (frac / 16f);
+					++offset;
+
+				}
+				// Light level
+				if (((header & 0x04) == 0x04) && (offset + 1) < data.length) {
+					++offset;
+					light = ((int) data[offset]) & 0xff;
+				}
+				// Battery
+				if (((header & 0x40) == 0x40) && (offset + 4) < data.length) {
+					++offset;
+					battery_volt = (data[offset] << 8) & 0xff00;
+					battery_volt += data[offset + 1] & 0xff;
+					battery_joule = (data[offset + 2] << 8) & 0xff00;
+					battery_joule += data[offset + 3] & 0xff;
+					offset += 3;
+				}
+			} catch (Exception e) {
+				log.error("HDR: {}, DL: {}, OFF: {}",
+						Integer.toString(header & 0xff, 16), data.length,
+						offset);
+			}
+
+			StringBuilder sb = new StringBuilder();
+			if (temp7 > -300) {
+				sb.append(isBool).append(',').append(temp7);
+			} else {
+				sb.append(',');
+			}
+			sb.append(',');
+			if (temp16 > -300) {
+				sb.append(String.format(THREE_DECIMAL_FLOAT, temp16));
+			}
+			sb.append(',');
+			if (light >= 0) {
+				sb.append(light);
+			}
+			sb.append(',');
+			if (battery_joule >= 0) {
+				sb.append(String.format(THREE_DECIMAL_FLOAT, battery_volt/1000f))
+						.append(',').append(battery_joule);
+			} else {
+				sb.append(',');
+			}
+
+			return sb.toString();
+		}
+	}
+
 	/**
 	 * Logging facility for this class.
 	 */
 	private static final Logger log = org.slf4j.LoggerFactory
 			.getLogger(GSSToCSV.class);
-	
+
 	private static final String THREE_DECIMAL_FLOAT = "%1.3f";
 
 	/**
@@ -81,8 +195,8 @@ public class GSSToCSV {
 		File gssFile = new File(args[0]);
 		File csvFile = new File(args[1]);
 
-    log.info("Input file: {}", args[0]);
-    log.info("Output file: {}", args[1]);
+		log.info("Input file: {}", args[0]);
+		log.info("Output file: {}", args[1]);
 
 		boolean forceOverwrite = false;
 
@@ -117,7 +231,6 @@ public class GSSToCSV {
 			return false;
 		}
 
-
 		if (csvFile.exists()) {
 			if (forceOverwrite) {
 				log.info("Overwriting existing file \"" + csvFile.getName()
@@ -145,13 +258,14 @@ public class GSSToCSV {
 			return false;
 		}
 		long fileLength = gssFile.length();
-		ArrayBlockingQueue<ReplaySample> sampleQueue = new ArrayBlockingQueue<ReplaySample>(1024, false);
-		
+		ArrayBlockingQueue<ReplaySample> sampleQueue = new ArrayBlockingQueue<ReplaySample>(
+				1024, false);
+
 		GSSReader reader = new GSSReader(gssIn, sampleQueue, fileLength);
 		CSVWriter writer = new CSVWriter(csvOut, sampleQueue);
 		reader.start();
 		writer.start();
-		
+
 		try {
 			reader.join();
 			writer.join();
@@ -159,7 +273,6 @@ public class GSSToCSV {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 
 		try {
 			gssIn.close();
@@ -175,6 +288,8 @@ public class GSSToCSV {
 	private static class CSVWriter extends Thread {
 		private static final Logger log = LoggerFactory
 				.getLogger(CSVWriter.class);
+
+		private static final PipSamplePrinter pipPrinter = new PipSamplePrinter();
 
 		private final PrintWriter output;
 		private final BlockingQueue<ReplaySample> queue;
@@ -194,62 +309,71 @@ public class GSSToCSV {
 
 		public void run() {
 			this.output
-					.write("\"Offset (ms)\",\"Receiver Timestamp (ms)\",\"Physical Layer\",\"Device ID\",\"Receiver ID\",\"RSSI\",\"Received Data\"\n");
+					.write("\"Offset (ms)\",\"Receiver Timestamp (ms)\",\"Physical Layer\",\"Device ID\",\"Receiver ID\",\"RSSI\",\"Binary\",\"Temp7\",\"Temp16\",\"Light\",\"Battery Voltage\",\"Battery Joules\"\n");
 
 			ReplaySample message = null;
-      long recordsWritten = 0l;
-      long lastTime = System.currentTimeMillis();
-      long now = 0l;
-      long timeDiff = 0l;
-      long lastRecords = 0l;
-      long recordDiff = 0l;
-      double recordRate = 0;
+			long recordsWritten = 0l;
+			long lastTime = System.currentTimeMillis();
+			long now = 0l;
+			long timeDiff = 0l;
+			long lastRecords = 0l;
+			long recordDiff = 0l;
+			double recordRate = 0;
 			while (this.keepRunning) {
 				try {
-					message = this.queue.poll(10,TimeUnit.SECONDS);
+					message = this.queue.poll(2, TimeUnit.SECONDS);
 				} catch (InterruptedException e) {
 					log.error("Unable to retrieve sample from queue.");
 					break;
 				}
-        if(message == null){
-          log.info("No more samples to convert.");
-          break;
-        }
-				this.output.write(Long.toString(message.getReplayTime()));
-				this.output.write(',');
-				this.output
-						.write(Long.toString(message.getReceiverTimeStamp()));
-				this.output.write(',');
-				this.output
-						.write(Integer.toString(message.getPhysicalLayer() & 0xFF));
-				this.output.write(',');
-				this.output.write(NumericUtils.toHexShortString(message.getDeviceId()));
-				this.output.write(',');
-				this.output.write(NumericUtils.toHexShortString(message.getReceiverId()));
-				this.output.write(',');
-				this.output.write(String.format(THREE_DECIMAL_FLOAT,message.getRssi()));
-				this.output.write(',');
-				if (message.getSensedData() != null) {
-					this.output.write(NumericUtils.toHexString(message.getSensedData()));
+				if (message == null) {
+					log.info("No more samples to convert.");
+					break;
+				}
+				if (message.getPhysicalLayer() == SampleMessage.PHYSICAL_LAYER_PIPSQUEAK) {
+					this.output.write(pipPrinter.sampleToString(message));
+				} else {
+					this.output.write(Long.toString(message.getReplayTime()));
+					this.output.write(',');
+					this.output.write(Long.toString(message
+							.getReceiverTimeStamp()));
+					this.output.write(',');
+					this.output.write(Integer.toString(message
+							.getPhysicalLayer() & 0xFF));
+					this.output.write(',');
+					this.output.write(NumericUtils.toHexShortString(message
+							.getDeviceId()));
+					this.output.write(',');
+					this.output.write(NumericUtils.toHexShortString(message
+							.getReceiverId()));
+					this.output.write(',');
+					this.output.write(String.format(THREE_DECIMAL_FLOAT,
+							message.getRssi()));
+					this.output.write(',');
+					if (message.getSensedData() != null) {
+						this.output.write(NumericUtils.toHexString(message
+								.getSensedData()));
+					}
 				}
 				this.output.write('\n');
-        ++recordsWritten;
+				++recordsWritten;
 
-        if((recordsWritten & 0xFFFFF) == 0 && log.isDebugEnabled()){
-          now = System.currentTimeMillis();
-          timeDiff = now - lastTime;
-          lastTime = now;
+				if ((recordsWritten & 0xFFFFF) == 0 && log.isDebugEnabled()) {
+					now = System.currentTimeMillis();
+					timeDiff = now - lastTime;
+					lastTime = now;
 
-          recordDiff = recordsWritten - lastRecords;
-          lastRecords = recordsWritten;
+					recordDiff = recordsWritten - lastRecords;
+					lastRecords = recordsWritten;
 
-          recordRate = ((double)(recordDiff)/timeDiff)*1000;
+					recordRate = ((double) (recordDiff) / timeDiff) * 1000;
 
-
-            log.debug(String.format("Translated %,d records. (%,.1f R/s)", recordsWritten,recordRate));
-        }
+					log.info(String.format(
+							"Translated %,d records. (%,.1f R/s)",
+							recordsWritten, recordRate));
+				}
 			}
-      log.info("Wrote {} records.", recordsWritten);
+			log.info("Wrote {} records.", recordsWritten);
 			this.output.flush();
 		}
 	}
@@ -297,10 +421,10 @@ public class GSSToCSV {
 			long lastByte = 0l;
 			long lastTime = System.currentTimeMillis();
 			long diffTime = 0l, now = 0l;
-			
+
 			double byteRate = 0;
 			while (this.keepRunning && (bytesRead < this.totalStreamBytes)) {
-			  message = new ReplaySample();
+				message = new ReplaySample();
 				int amtRead = getNextMessage(this.input, message);
 				if (amtRead < 0) {
 					log.error("Unable to read a sample from the input file.");
@@ -313,20 +437,23 @@ public class GSSToCSV {
 					log.error("Unable to insert {}", message);
 					continue;
 				}
-        if(log.isDebugEnabled()){
-				 ++recordsRead;
-			      if((recordsRead & 0xFFFFF) == 0){ 
-			    	now = System.currentTimeMillis();
-			        diffTime = now - lastTime;
-			        lastTime = now;
-			        
-			        byteRate = (double)(bytesRead - lastByte)/diffTime;
-			        lastByte = bytesRead;
-			        log.debug(String.format("%.4f%% complete. (%.2f KB/s)", ((double)(bytesRead) / this.totalStreamBytes)*100, byteRate));
-			      }
+				if (log.isDebugEnabled()) {
+					++recordsRead;
+					if ((recordsRead & 0xFFFFF) == 0) {
+						now = System.currentTimeMillis();
+						diffTime = now - lastTime;
+						lastTime = now;
+
+						byteRate = (double) (bytesRead - lastByte) / diffTime;
+						lastByte = bytesRead;
+						log.debug(String
+								.format("%.4f%% complete. (%.2f KB/s)",
+										((double) (bytesRead) / this.totalStreamBytes) * 100,
+										byteRate));
+					}
 				}
 			}
-      log.info("Completed reading all {} samples.", recordsRead);
+			log.info("Completed reading all {} samples.", recordsRead);
 		}
 
 		/**
@@ -369,7 +496,7 @@ public class GSSToCSV {
 						byte[] data = new byte[messageLength];
 						inputStream.readFully(data);
 						message.setSensedData(data);
-					} 
+					}
 
 				} catch (IOException ioe) {
 					log.error(
